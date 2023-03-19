c48177d2071f4d0a1c28f631382101bacb3cac82
theburningmonk <theburningmonk@gmail.com>
2023-03-19 15:16:50 +0100

    feat: no retries for magic links

--- functions/define-auth-challenge.js
+++ functions/define-auth-challenge.js
@@ -7,18 +7,23 @@ module.exports.handler = async (event) => {
     return event
   }
 
-  const lastAttempt = _.last(event.request.session)
-  if (lastAttempt?.challengeResult === true) {
-    // User gave right answer
-    event.response.issueTokens = true
+  if (_.isEmpty(event.request.session)) {
+    // Issue new challenge
+    event.response.issueTokens = false
     event.response.failAuthentication = false
-    return event
+    event.response.challengeName = 'CUSTOM_CHALLENGE'
+  } else {
+    const lastAttempt = _.last(event.request.session)
+    if (lastAttempt.challengeResult === true) {
+      // User gave right answer
+      event.response.issueTokens = true
+      event.response.failAuthentication = false
+    } else {
+      // User gave wrong answer
+      event.response.issueTokens = false
+      event.response.failAuthentication = true
+    }
   }
-
-  // Issue new challenge
-  event.response.issueTokens = false;
-  event.response.failAuthentication = false;
-  event.response.challengeName = 'CUSTOM_CHALLENGE';
 
   return event
 }
